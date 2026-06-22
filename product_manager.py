930ff2d1876a3a45782d0cfa0859f3171d2f87c3
Rebeca Lucus <rebecaflucus@gmail.com>
2026-06-22 15:23:56 +0300

    Adaugare clasa ProductManager

---
+++ product_manager.py
@@ -0,0 +1,18 @@
+from product import Product
+
+class ProductManager:
+    def __init__(self):
+        self.products = []
+    
+    def add_product(self, product):
+        self.products.append(product)
+
+    def show_products(self):
+        for product in self.products:
+            product.display_info()
+    def total_value(self):
+        total = 0
+        for product in self.products:
+            total += product.price * product.quantity
+
+        print("Valoare totala:", total)
