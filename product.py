72dfc4aed077f71346e24e256487dbb940c5a0ed
Rebeca Lucus <rebecaflucus@gmail.com>
2026-06-22 15:07:09 +0300

    Creare clasa product.py

---
+++ product.py
@@ -0,0 +1,21 @@
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def display_info(self):
+        print(f"Produs: {self.name}")
+        print(f"Pret: {self.price} lei")
+        print(f"Cantitate: {self.quantity}")
+        
+    def update_quatity(self, amount):
+        if amount < 0:
+            if self.quantity >= -amount:
+                self.quantity += amount
+                print(f"Stoc actualizat: {self.quantity}")
+            else:
+                print("Nu exista stoc suficient!")
+        else:
+            self.quantity += amount
+            print(f"Stoc actualizat: {self.quantity}")
